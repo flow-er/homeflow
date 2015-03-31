77b8424d58882713cc3f687438a2d2307a9c7c7d
Jongho Lim <sloth@kookmin.ac.kr>
2015-03-31 22:56:41 +0900

    to remove the Android files for rebuilding of the UI.

--- src/app_android/doc/html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['homeflow_20android_20app',['HomeFlow android app',['../index.html',1,'']]]
-];
--- src/app_android/doc/html/search/classes_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['dashboardactivity',['DashboardActivity',['../classkookmin_1_1cs_1_1homeflow_1_1_dashboard_activity.html',1,'kookmin::cs::homeflow']]],
-  ['device',['Device',['../classkookmin_1_1cs_1_1homeflow_1_1data_1_1_device.html',1,'kookmin::cs::homeflow::data']]],
-  ['devicelistactivity',['DeviceListActivity',['../classkookmin_1_1cs_1_1homeflow_1_1_device_list_activity.html',1,'kookmin::cs::homeflow']]]
-];
